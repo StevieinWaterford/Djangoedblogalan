91b212628ec707f2ba403e6b9964818315c63a66
StevieinWaterford <stevieindub@gmail.com>
2021-11-03 16:08:40 +0000

    deployment commit

---
+++ codestar/__pycache__/env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://krbofizawdhkae:159ed49a006212ccc52a2ac4a7f890bf789151ae0b919d3f71c25ef4764d94f4@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/d726aie06cp1nb"
+os.environ["SECRET_KEY"] = "randomSecretKey2021!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
---
+++ codestar/env.py
@@ -0,0 +1,4 @@
+import os
+ os.environ ["DATABASE_URL"] = "postgres://krbofizawdhkae:159ed49a006212ccc52a2ac4a7f890bf789151ae0b919d3f71c25ef4764d94f4@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/d726aie06cp1nb"
+ os.environ ["SECRET_KEY"] = "randomSecretKey2021!!"
+ 
